3137489e818970bdd12e053a28e0e69536591da1
tindo24 <tindo24@gmail.com>
2014-09-17 15:16:27 -0400

    files for project 1

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+<title>About ME </title>
+<link rel="stylesheet" href="css/blueprint/screen.css" media="screen, projection">
+<link rel="stylesheet" href="css/blueprint/print.css" media="print">
+
+<link rel="stylesheet" href="css/style.css" type="text/css" />
+
+    <link rel="stylesheet"
+          href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/hot-sneaks/jquery-ui.css"
+          type="text/css" media="all" />
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"
+            type="text/javascript"></script>
+    <script
+            src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"
+            type="text/javascript"></script>
+    <script type="text/javascript">
+        $(function() {
+            $( "#accordion" ).accordion();
+        });
+    </script>
+</head>
+<body>
+<div class="container">
+<!-- 
+This is just a starting point.  Use the header, footer, three navigation modes, and content as a starting point.  You will need to edit this document! 
+-->
+
+<!-- masthead -->
+<header>
+<h1 id="logo" class="span-24"> About Me page</h1>
+</header>
+<!-- end masthead -->
+
+<!-- site navigation -->
+<nav id="top-nav">
+<ul>
+<h2> Philip Ondiege Dynamic Web Application Portfolio</h2>
+
+</ul>
+</nav>
+<!-- end site navigation -->
+
+<!-- category navigation -->
+<nav id="cat-nav">
+<ul  class="span-7">
+<li class="youarehere">Project links
+<ul><li><a href="#">Project 2 </a><li><a href="#">Project 2 Github link </a></li></li>
+
+    <li></li><a href="#">Project 3</a><li><a href="#">Project 3 Github link </a></li></li>
+   <li> <a href="#">Project 4</a><li><a href="#">Project 4 Github link </a></li></li>
+
+</ul>
+</li>
+</ul>
+<!-- end category navigation -->
+</nav>
+<!-- breadcrumb navigation -->
+<nav id="breadcrumb">
+<ul class="span-15"><li><a href="#">Philip Ondiege</a><ul><li><a href="#">Dynamic Web Application</a><ul><li><a href="#"></a> Me Page</li></ul></li></ul></li></ul>
+<!-- end breadcrumb navigation -->
+</nav>
+
+<section id="content" class="span-16">
+    <img src="images/PhilipProImage.jpg" alt="My Picture" height="100" width="100">
+
+  <div id= "accordion">
+
+
+<h2>About Me</h2>
+      <div>
+<p>  I'm a data analyst by profession. My interest are in
+    <li>Business Intelligence</li>
+    <li>Data analytics </li>
+    <li>big data.</li>
+</p>
+    <p>We  just moved to Boston in August. I'm looking into applying for a masters in Information Management in the spring semester. I enjoy playing basketball and Biking.
+    One of my goals is to run the Boston Marathon in the next two years.</p></div>
+<h2>Why I'm taking this course ?</h2>
+    <div><p> I have interest in web programming and web development.
+    I'm hoping to gain a deeper understanding of the MVC framework in relation to PHP </p></div>
+
+
+<h2>Previous Experience with Programming</h2>
+    <div><p>Microsoft VB. <br> Microsoft SQL Server  </p></div>
+
+    <h2>Computer that I'm working on</h2>
+
+    <div> <p>Mac</p></div>
+
+    </div>
+
+</section>
+<hr class="clearme">
+<footer class="span-7">
+<p>Contact<br/>ondiege01@g.harvard.edu<br/>Newton, MA 02458</p>
+</footer>
+
+
+</div>
+
+</body>
+</html>
